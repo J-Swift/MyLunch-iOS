5bd672ad0ed8230bcb272938e03ed50a724b136b
Jimmy Reichley <jimmyqpublik@gmail.com>
2015-02-26 00:11:09 -0500

    Basic photo selection

--- MyLunch/MyLunch/MYLSelectPhotosViewController.m
+++ MyLunch/MyLunch/MYLSelectPhotosViewController.m
@@ -9,6 +9,9 @@
 #import "MYLSelectPhotosViewController.h"
 
 #import "MYLDetailViewController.h"
+#import "MYLLunchModel.h"
+
+#import <ELCImagePickerController/ELCImagePickerController.h>
 
 
 static const CGFloat kViewMargin = 10.0f;
@@ -16,9 +19,10 @@ static const CGFloat kSectionVSpacer = 20.0f;
 static const CGFloat kItemVSpacer = 5.0f;
 
 
-@interface MYLSelectPhotosViewController ()
+@interface MYLSelectPhotosViewController () <ELCImagePickerControllerDelegate>
 
 @property (nonatomic, weak) UILabel *instructionLabel;
+@property (nonatomic, weak) UILabel *debugLabel;
 
 @end
 
@@ -42,14 +46,23 @@ static const CGFloat kItemVSpacer = 5.0f;
   view.backgroundColor = [UIColor whiteColor];
   
   UILabel *instructionLabel = [UILabel new];
-  instructionLabel.text = @"Add photos to your lunch (optional)";
+  instructionLabel.text = @"Tap to add photos to your lunch (optional)";
   instructionLabel.numberOfLines = 0;
   [view addSubview:instructionLabel];
   _instructionLabel = instructionLabel;
   
+  UILabel *debugLabel = [UILabel new];
+  debugLabel.text = @"";
+  debugLabel.numberOfLines = 0;
+  [view addSubview:debugLabel];
+  _debugLabel = debugLabel;
+  
   self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone
                                                                                          target:self
                                                                                          action:@selector(donePressed)];
+  
+  [view addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self
+                                                                     action:@selector(beginSelectingPhotos)]];
   self.view = view;
 }
 
@@ -64,6 +77,12 @@ static const CGFloat kItemVSpacer = 5.0f;
   frame.size = [self.instructionLabel sizeThatFits:CGSizeMake(width, CGFLOAT_MAX)];
   frame.origin = CGPointMake(kViewMargin, kViewMargin);
   self.instructionLabel.frame = frame;
+  
+  self.debugLabel.text = [NSString stringWithFormat:@"%@ images", @([self.model.images count])];
+  frame = self.debugLabel.frame;
+  frame.size = [self.debugLabel sizeThatFits:CGSizeMake(width, CGFLOAT_MAX)];
+  frame.origin = CGPointMake(kViewMargin, CGRectGetMaxY(self.instructionLabel.frame) + kSectionVSpacer);
+  self.debugLabel.frame = frame;
 }
 
 - (NSString *)title
@@ -71,7 +90,35 @@ static const CGFloat kItemVSpacer = 5.0f;
   return @"Add Photos";
 }
 
+#pragma mark - ELCImagePickerControllerDelegate
+
+- (void)elcImagePickerController:(ELCImagePickerController *)picker didFinishPickingMediaWithInfo:(NSArray *)info
+{
+  NSMutableArray *images = [NSMutableArray array];
+  [info enumerateObjectsUsingBlock:^(NSDictionary *imageInfo, NSUInteger idx, BOOL *stop) {
+    UIImage *image = imageInfo[@"UIImagePickerControllerOriginalImage"];
+    [images addObject:image];
+  }];
+  [self.model associateImages:images];
+  
+  [self.view setNeedsLayout];
+  [self dismissViewControllerAnimated:YES completion:NULL];
+}
+
+- (void)elcImagePickerControllerDidCancel:(ELCImagePickerController *)picker
+{
+  [self dismissViewControllerAnimated:YES completion:NULL];
+}
+
 #pragma mark - Helpers
+
+- (void)beginSelectingPhotos
+{
+  ELCImagePickerController *imagePickerVC = [ELCImagePickerController new];
+  imagePickerVC.maximumImagesCount = 4;
+  imagePickerVC.imagePickerDelegate = self;
+  [self presentViewController:imagePickerVC animated:YES completion:NULL];
+}
 
 - (void)donePressed
 {
