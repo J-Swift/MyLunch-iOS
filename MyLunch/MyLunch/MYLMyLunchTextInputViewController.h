dec0e06587c5ee46caf5fcebc3693aa17b5a61c2
Jimmy Reichley <jimmyqpublik@gmail.com>
2015-02-25 21:26:37 -0500

    No storyboards/xibs; all views are done programmatically

--- MyLunch/MyLunch/MYLMyLunchTextInputViewController.h
+++ MyLunch/MyLunch/MYLMyLunchTextInputViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  MYLMyLunchTextInputViewController.h
 //  MyLunch
 //
 //  Created by James Reichley on 2/25/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
-
+@interface MYLMyLunchTextInputViewController : UIViewController
 @end
 
---
+++ MyLunch/MyLunch/MYLMyLunchTextInputViewController.m
@@ -0,0 +1,27 @@
+//
+//  MYLMyLunchTextInputViewController
+//  MyLunch
+//
+//  Created by James Reichley on 2/25/15.
+//  Copyright (c) 2015 RadReichley. All rights reserved.
+//
+
+#import "MYLMyLunchTextInputViewController.h"
+
+@interface MYLMyLunchTextInputViewController ()
+@end
+
+@implementation MYLMyLunchTextInputViewController
+
+- (void)loadView
+{
+  UIView *view = [UIView new];
+  self.view = view;
+}
+
+- (NSString *)title
+{
+  return @"My Lunch";
+}
+
+@end
